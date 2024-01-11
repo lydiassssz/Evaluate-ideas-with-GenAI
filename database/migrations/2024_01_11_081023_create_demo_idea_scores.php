a88363058cfb4013f3a2092c62074298a4503c09
lydia <s6023143@st.shiga-u.ac.jp>
2024-01-11 17:30:32 +0900

    migrateファイルの追記

--- database/migrations/2024_01_11_081023_create_demo_idea_scores.php
+++ database/migrations/2024_01_11_081023_create_demo_idea_scores.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('demo_idea_scores', function (Blueprint $table) {
             $table->id();
+            $table->text('problem')->comment('problem')->nullable();
+            $table->text('solution')->comment('solution')->nullable();
+            $table->integer('evidence')->comment('evidence_score')->default(0);
+            $table->text('evidence_justification')->comment('evidence_justification')->nullable();
+            $table->text('evidence_detail')->comment('evidence_detail')->nullable();
+            $table->integer('impact')->comment('impact_score')->default(0);
+            $table->text('impact_justification')->comment('impact_justification')->nullable();
+            $table->text('impact_detail')->comment('impact_detail')->nullable();
+            $table->integer('possible')->comment('possible_score')->default(0);
+            $table->text('possible_justification')->comment('possible_justification')->nullable();
+            $table->text('possible_detail')->comment('possible_detail')->nullable();
+            $table->boolean('length')->comment('length')->default(0);
+            $table->text('note')->comment('note')->nullable();
             $table->timestamps();
         });
     }
