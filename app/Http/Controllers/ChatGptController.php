a00f5f2f2f656e8f03d0b860b0fb53cb971ca1db
lydia <s6023143@st.shiga-u.ac.jp>
2024-01-12 22:37:24 +0900

    csv入力機能の実装80

--- app/Http/Controllers/ChatGptController.php
+++ app/Http/Controllers/ChatGptController.php
@@ -40,6 +40,7 @@ class ChatGptController extends Controller
     {
 
         $data = DemoIdeaScore::find($id);
+        dd($res_data);
         if(isset($res_data->Evidence->Score)){
             $data->evidence = $res_data->Evidence->Score;
         } else {$data->evidence = 7; }
