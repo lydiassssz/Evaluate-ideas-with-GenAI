090235f1397bdf97e263b17742df1c42cb0f2080
lydia <s6023143@st.shiga-u.ac.jp>
2024-01-12 21:25:13 +0900

    csv入力機能の実装49

--- app/Http/Controllers/ChatGptController.php
+++ app/Http/Controllers/ChatGptController.php
@@ -159,7 +159,7 @@ class ChatGptController extends Controller
         for($count=1; $count < 3; $count++){
             $response = curl_exec($ch);
             $response =json_decode($response, true);
-            $res = json_decode($response['choices'][0]['message'], true);
+            $res = $response['choices'][0]['message'];
             $res = $res['content'];
 
             $pattern = '/{.*}/s';
