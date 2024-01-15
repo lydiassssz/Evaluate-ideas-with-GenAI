78e0abf29341a115c2a324bf3207b84118ed5574
lydia <s6023143@st.shiga-u.ac.jp>
2024-01-15 19:32:42 +0900

    chatGPTの応答が正常かのテスト07

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +23,9 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
+        // レンサバの設定と相性が悪く、https でアクセスしているにも関わらず $_SERVER['HTTPS] が設定されず、
+        // Laravel, Symfony 側の処理で http 始まりの URL で処理されてしまう。
+        // config('app.url') が https 始まりか？http 始まりか？ をもとに強制的に scheme を設定する
+        URL::forceScheme(\Str::startsWith(config('app.url'), 'https') ? 'https' : 'http');
     }
 }
